8617da505bb20c3839bd5b189d441c4135d65c72
nut-3 <kshurukhin@gmail.com>
2021-06-12 23:02:19 +0300

    Implement meal to user relationship end

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -7,14 +7,14 @@ import java.util.Collection;
 
 public interface MealRepository {
     // null if updated meal do not belong to userId
-    Meal save(Meal meal);
+    Meal save(Meal meal, User user);
 
     // false if meal do not belong to userId
-    boolean delete(int id);
+    boolean delete(int id, User user);
 
     // null if meal do not belong to userId
-    Meal get(int id);
+    Meal get(int id, User user);
 
     // ORDERED dateTime desc
-    Collection<Meal> getAll();
+    Collection<Meal> getAll(User user);
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.inmemory;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
@@ -14,32 +15,41 @@ import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> this.save(meal, new User(null, null, null, null, null)));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, User user) {
+        Map<Integer, Meal> userMeal = repository.computeIfAbsent(user.getId(), integer -> new ConcurrentHashMap<>());
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            userMeal.put(meal.getId(), meal);
             return meal;
         }
+        if (repository.entrySet().stream()
+                .anyMatch(integerMapEntry -> integerMapEntry.getValue()
+                        .containsKey(meal.getId()) &&
+                        !integerMapEntry.getKey().equals(user.getId()))) {
+            return null;
+        }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return userMeal.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int id, User user) {
+        return repository.computeIfAbsent(user.getId(), integer -> new ConcurrentHashMap<>())
+                .remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int id, User user) {
+        return repository.computeIfAbsent(user.getId(), integer -> new ConcurrentHashMap<>())
+                .get(id);
     }
 
     @Override
